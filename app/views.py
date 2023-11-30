2956a03c09ff683e3a8c7efe9d2143f188c62d50
PrasannaUL <prasannareddy15b3@gmail.com>
2023-11-30 12:08:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def bootstrap_download(request):
+    return render(request,'bootstrap_download.html')
+
+
+def bootstrap_download1(request):
+    return render(request,'bootstrap_download1.html')
+
+
